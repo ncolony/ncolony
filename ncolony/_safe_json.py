e61db1f8a7b74a5304bc358d3fde8e9b222a031e
Moshe Zadka <zadka.moshe@gmail.com>
2017-11-05 13:23:49 -0800

    frop

---
+++ ncolony/_safe_json.py
@@ -0,0 +1,14 @@
+SCHEMA_METADATA = object()
+
+def schemattr(schema, default=attr.NOTHING):
+    
+
+def load(fname, cls):
+    with io.open(fname, encoding='utf-8') as fp:
+        content = fp.read()
+    parsed = json.loads(content)
+    
+    
+
+def dumps(
+    
